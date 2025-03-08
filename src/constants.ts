df724a18d60bb9a089cb470a60718719eb13f8fb
Marcos Sastre <marcossastre73@gmail.com>
2025-03-08 15:56:05 +1100

    Test mode functionality

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const RENSHUU_TOKEN_KEY = 'rpgtkn';
 export const RENSHUU_PROMPT_CONFIG_KEY = 'rpgcfg';
+export const TEST_MODE_TOKEN = 'b075aec88a1d0a8ccd4352e3e7b87a333db18b2e2fdc3b9f376586faa85db5d6'
--- src/layout/Shell.tsx
+++ src/layout/Shell.tsx
@@ -1,24 +1,43 @@
-import { AppShell, Button, Group, Title } from '@mantine/core';
-
+import { Alert, Anchor, AppShell,  Stack, } from '@mantine/core';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { TEST_MODE_TOKEN } from '../constants';
+import { IconInfoCircle } from '@tabler/icons-react';
 interface ShellProps {
   children: React.ReactNode;
 }
 
 export function Shell({ children }: ShellProps) {
+
+  const token = useSelector((state: RootState) => state.token.value);
   return (
     <AppShell
-      header={{ height: 60 }}
+      header={{ height: 0 }}
       padding="md"
     >
-      <AppShell.Header>
+      {/* <AppShell.Header>
         <Group h="100%" px="md" justify="space-between">
           <Title order={2}>App Name</Title>
           <Button>Action</Button>
         </Group>
-      </AppShell.Header>
+      </AppShell.Header> */}
 
       <AppShell.Main bg="gray.0">
-        {children}
+        <Stack >
+          {token && token === TEST_MODE_TOKEN && (
+            <Stack w="100%" bg="gray.0" pos="sticky" top={0} style={{ zIndex: 100 }}>
+              <Alert
+                className="test-mode-alert"
+                variant="light"
+                color="yellow"
+                title="Test mode"
+                icon={<IconInfoCircle />}>
+                You are in TEST mode. <Anchor>Exit test mode</Anchor> to connect to your Renshuu account.
+              </Alert>
+            </Stack>
+          )}
+          {children}
+        </Stack>
       </AppShell.Main>
     </AppShell>
   );
